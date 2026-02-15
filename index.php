936e1f3466a746d9705b1d2a7e8f051c8bd34b20
Rahul Dhiman <rahul.dhiman.mohanlal@gmail.com>
2026-02-15 17:44:46 +0530

    updated

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php include 'includes/header.php'; ?>
 
+<!-- Categories Section -->
+<link rel="stylesheet" href="assets/css/categories.css">
+<?php include 'components/categories.php'; ?>
+
 <!-- Hero Section -->
 <link rel="stylesheet" href="assets/css/hero.css">
 <?php include 'components/hero.php'; ?>
